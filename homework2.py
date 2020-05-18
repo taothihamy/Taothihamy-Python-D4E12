60c0e5ec579710e6a87db47f2cfde66628c106fb
taothihamy <65484725+taothihamy@users.noreply.github.com>
2020-05-18 21:03:33 +0700

    .

---
+++ homework2.py
@@ -0,0 +1,55 @@
+# from random import randint
+# random_num = randint(0, 100)
+# if random < 30:
+#     print("Bạn trông bình thường")
+# elif random_num <30 
+
+
+# Bài toán BMI
+# height = int(input("Enter height in centimeters: "))
+# weight = int(input("Enter weight in kg: "))
+
+# bmi = weight/((height/100)**2) 
+
+# if bmi < 16:
+#    print("severely underweight")
+
+# elif bmi >= 16 and bmi < 18.5:
+#    print("underweight")
+
+# elif bmi >= 18.5 and bmi < 25:
+#    print("Normal")
+
+# elif bmi >= 25 and bmi < 30:
+#    print("overweight")
+
+# elif bmi >=30:
+#    print("Obese")
+
+# Bài toán Factorial
+# n=int(input("Input a number to compute the factiorial : "))
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n * factorial(n-1)
+# print(factorial(n))
+
+# Bài tập turtle
+# from turtle import*
+# speed(-1)
+# for i in range(3,7):
+#     for j in range(i):
+#         forward(100)
+#         left(360/i)
+# mainloop()
+
+# Bài tập multiplication table
+# n = int(input("Enter a number"))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(j*i, end ="\t") 
+#     print("\n")  
+
+for i in range(20):
+    print(i)
